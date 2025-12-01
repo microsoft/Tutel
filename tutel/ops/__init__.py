5670b59fa283dd0ec38b72d654a821f0fd7f61c4
ghostplant <12099308+ghostplant@users.noreply.github.com>
2025-12-01 01:01:30 +0000

    add marlin conversion ops

--- tutel/ops/__init__.py
+++ tutel/ops/__init__.py
@@ -100,7 +100,153 @@ def from_mxfp4(p):
   return w
 
 
+import numpy as np
+
+def marlin_pack(w_unpacked):
+
+    def _get_marlin_perms(device):
+        perm = []
+        for i in range(32):
+            perm1 = []
+            col = i // 4
+            for block in [0, 1]:
+                for row in [
+                    2 * (i % 4),
+                    2 * (i % 4) + 1,
+                    2 * (i % 4 + 4),
+                    2 * (i % 4 + 4) + 1
+                ]:
+                    perm1.append(16 * row + col + 8 * block)
+            for j in range(4):
+                perm.extend([p + 256 * j for p in perm1])
+        perm = np.array(perm)
+        interleave = np.array([0, 2, 4, 6, 1, 3, 5, 7])
+        perm = perm.reshape((-1, 8))[:, interleave].ravel()
+        return torch.from_numpy(perm).long().to(device)
+
+    def unpack_weight_4d(packed_weight):
+        # only for 4-bit weight packed in 8-bit (such as 2 fp4 in one uint8)
+        device = packed_weight.device
+        # shape: [total_num_experts, out_feature, in_feature // groupsize, groupsize // 2]
+        E, N, G, half_groupsize = packed_weight.shape
+        w_bytes = packed_weight.view(E, N, G * half_groupsize)
+        low = w_bytes & 0x0F
+        high = (w_bytes >> 4) & 0x0F
+        # stack(..., dim=-1) -> [E, N, G*groupsize//2, 2] -> view -> [E, N, K]
+        w_unpacked = torch.stack([low, high], dim=-1).view(E, N, -1)
+        # Marlin [In, Out]，GPTOSS [Out, In]
+        return w_unpacked.transpose(1, 2).contiguous()
+
+    w_unpacked = unpack_weight_4d(w_unpacked)
+    device = w_unpacked.device
+    tile = 16
+    E, K, N = w_unpacked.shape
+    # Shape: [E, K, N] -> [E, K//16, 16, N//16, 16]
+    w = w_unpacked.reshape(E, K // tile, tile, N // tile, tile)
+    # Permute -> [E, K//16, N//16, 16, 16]
+    w = w.permute(0, 1, 3, 2, 4)
+    # Reshape -> [E, K//16, N * 16]
+    w = w.reshape(E, K // tile, N * tile)
+    perm = _get_marlin_perms(device)
+    num_cols = w.shape[-1]
+    assert num_cols % perm.numel() == 0
+    w_flat = w.reshape(-1, perm.numel())
+    w_permuted = w_flat[:, perm]
+    w = w_permuted.reshape(E, K // tile, N * tile)
+    # pack into Marlin int32
+    out_cols = w.shape[-1] // 8
+    marlin_weight = torch.zeros((E, K // tile, out_cols), dtype=torch.int32, device=device)
+    w = w.to(torch.int32)
+    for i in range(8):
+        marlin_weight |= (w[:, :, i::8] << (4 * i))
+    return marlin_weight
+
+
+def marlin_permute_scales(s, group_size):
+
+    def get_scale_perms():
+        scale_perm: list[int] = []
+        for i in range(8):
+            scale_perm.extend([i + 8 * j for j in range(8)])
+        scale_perm_single: list[int] = []
+        for i in range(4):
+            scale_perm_single.extend([2 * i + j for j in [0, 1, 8, 9, 16, 17, 24, 25]])
+        return scale_perm, scale_perm_single
+
+    def nvfp4_marlin_process_scales(marlin_scales):
+        if not (marlin_scales >= 0).all():
+            logger.warning_once(
+                "NVFP4 Marlin assumes the scales to be >=0, but has encountered "
+                "negative scales. Accuracy will likely be degraded. This is "
+                "because it changes the scales from FP8-S1E4M3 to a special "
+                "FP8-S0E5M3 format to speedup the dequantization."
+            )
+        # convert to half first, we would convert to fp8 later
+        marlin_scales = marlin_scales.to(torch.half)
+
+        # 8 is the number of scale number using by one thread
+        marlin_scales = marlin_scales.view(marlin_scales.size(0), marlin_scales.size(1) // 2, 2, -1, 8)
+        marlin_scales = marlin_scales.permute(0, 1, 3, 2, 4).reshape(
+            marlin_scales.size(0), marlin_scales.size(1) * 2, -1
+        )
+
+        # fit the layout of fp8 dequantization
+        marlin_scales = marlin_scales.view(marlin_scales.size(0), -1, 4)[:, :, [0, 2, 1, 3]].view(
+            marlin_scales.size(0), marlin_scales.size(1), -1
+        )
+
+        # We assume that weight_scale (FP8-S1E4M3) is always greater
+        # than or equal to 0. So we can convert
+        # (weight_scale * (2 ** 7) to a special FP8-S0E5M3 format.
+        # After multiplying by 2 ** 7, the top bit of FP8-S0E5M3 would always be 1
+        # when weight_scale > 0. This allows us to have an exponent bias
+        # closer to zero after dequantization.
+
+        marlin_scales = (marlin_scales * (2**7)).view(torch.int16) << 1
+        marlin_scales = marlin_scales.view(torch.float8_e4m3fn)
+        marlin_scales = marlin_scales[:, :, 1::2].contiguous()
+        return marlin_scales
+
+    def mxfp4_marlin_process_scales(marlin_scales):
+        marlin_scales = marlin_scales.view(marlin_scales.size(0), marlin_scales.size(1) // 2, 2, -1, 8)
+        marlin_scales = marlin_scales.permute(0, 1, 3, 2, 4).reshape(
+            marlin_scales.size(0), marlin_scales.size(1) * 2, -1
+        )
+        marlin_scales = marlin_scales.view(marlin_scales.size(0), -1, 4)[:, :, [0, 2, 1, 3]].view(
+            marlin_scales.size(0), marlin_scales.size(1), -1
+        )
+        marlin_scales = marlin_scales.to(torch.float8_e8m0fnu)
+        return marlin_scales
+
+    if group_size != 16:
+      s = s.view(torch.float8_e8m0fnu)
+    s = s.transpose(1, 2).to(torch.bfloat16)
+    size_k, size_n = s.size(-2) * group_size, s.size(-1)
+    scale_perm, scale_perm_single = get_scale_perms()
+    if group_size < size_k and group_size != -1:
+        s = s.reshape((s.size(0), -1, len(scale_perm)))[:, :, scale_perm]
+    else:
+        s = s.reshape((s.size(0), -1, len(scale_perm_single)))[:, :, scale_perm_single]
+    s = s.reshape((s.size(0), -1, size_n)).contiguous()
+    if group_size != 16:
+        s = mxfp4_marlin_process_scales(s)
+    else:
+        s = nvfp4_marlin_process_scales(s)
+    s.group_size = group_size
+    return s
+
+def marlin_nvfp4_process_global_scale(global_scale):
+    global_scale = global_scale.to(torch.bfloat16)
+    assert global_scale.dtype in [torch.half, torch.bfloat16]
+    fp4_exponent = 2
+    target_exponent = 8
+    # exponent_bias_fp16 = 2 ** 4 - 2 ** 1 = 14
+    # exponent_bias_bf16 = 2 ** 7 - 2 ** 1 = 126
+    exponent_bias = 2 ** (target_exponent - 1) - 2 ** (fp4_exponent - 1)
+    return global_scale * (2.0 ** (exponent_bias - 7))
+
+
 def __getattr__(name):
-    fn = getattr(torch.ops.tutel_ops, name)
-    return torch.compiler.disable(fn, recursive=True)
+  fn = getattr(torch.ops.tutel_ops, name)
+  return torch.compiler.disable(fn, recursive=True)
 
